2c39259f974a2082c009401f4a45c03d293ac0de
dilli babu <dilli4904@gmail.com>
2024-07-05 15:49:09 +0530

    removed unwanted pages

--- src/components/routes/MyRoutes.js
+++ src/components/routes/MyRoutes.js
@@ -1,17 +1,11 @@
 import Login from "../defaultPage/login";
 import Layout from "../layout/layout";
 import Products from "../pages/products";
-import Category from "../pages/category";
-import Units from "../pages/units";
-import Inventory from "../pages/Inventory";
 
 export const ROUTES = {
   LOGIN: "/",
   LAYOUT: "layout",
   PRODUCTS: "/products",
-  CATEGORY: "/category",
-  UNITS: "/units",
-  INVENTORY: "/inventory",
 };
 
 const my_routes = [
@@ -34,27 +28,6 @@ const my_routes = [
         key: "Products",
         authenticate: false,
         component: <Products />,
-      },
-      {
-        path: ROUTES.CATEGORY,
-        name: "Category",
-        key: "Category",
-        authenticate: false,
-        component: <Category />,
-      },
-      {
-        path: ROUTES.UNITS,
-        name: "Units",
-        key: "Units",
-        authenticate: false,
-        component: <Units />,
-      },
-      {
-        path: ROUTES.INVENTORY,
-        name: "Units",
-        key: "Units",
-        authenticate: false,
-        component: <Inventory />,
       },
     ],
   },
