17143124f62de43b2bafaf1b6b1a2b85f6fac8e6
dilli babu <dilli4904@gmail.com>
2024-07-06 16:28:26 +0530

    inital push

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,97 +1,46 @@
-import React, { useState } from "react";
-import { Layout, Menu, Button, theme } from "antd";
+// components/layout/Layout.js
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { Link } from 'react-router-dom';
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
   UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import { ROUTES } from "../routes/MyRoutes";
-import { Link } from "react-router-dom";
-import SubMenu from "antd/es/menu/SubMenu";
+  LaptopOutlined,
+  NotificationOutlined,
+} from '@ant-design/icons';
 
-const { Header, Sider, Content } = Layout;
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 const CustomLayout = ({ children }) => {
-  const [collapsed, setCollapsed] = useState(false);
-  const { colorBgContainer, borderRadiusLG } = theme.useToken().token;
-
-  const menuItems = [
-   {
-    icon: <UserOutlined />,
-   label: "Inventory",
-   key: "Inventory",
-   children:[
-    {
-      key: "1",
-      icon: <UserOutlined />,
-      label: "Products",
-      url: ROUTES.PRODUCTS,
-    },
-   ]}, 
-   
-    {
-      key: "3",
-      icon: <UploadOutlined />,
-      label: "Invoice",
-      url: ROUTES.LOGIN,
-    },
-  
-   
-  ];
-
   return (
-    <Layout>
-    <Sider width={200} className="site-layout-background">
-      <Menu
-        mode="inline"
-        defaultSelectedKeys={['1']}
-        defaultOpenKeys={['Products']}
-        style={{ height: '100%', borderRight: 0 ,backgroundColor:"lightskyblue"}}
-      >
-        {menuItems.map(item => {
-          if (item.children) {
-            return (
-              <SubMenu key={item.key} icon={item.icon} title={item.label}>
-                {item.children.map(child => (
-                  <Menu.Item key={child.key}>
-                    <Link to={child.url}>{child.label}</Link>
-                  </Menu.Item>
-                ))}
-              </SubMenu>
-            );
-          } else {
-            return (
-              <Menu.Item key={item.key} icon={item.icon}>
-                <Link to={item.url}>{item.label}</Link>
-              </Menu.Item>
-            );
-          }
-        })}
-      </Menu>
-    </Sider>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            <Link to="/admin">Dash Board</Link>
+          </Menu.Item>
+          <Menu.Item key="2" icon={<LaptopOutlined />}>
+            <Link to="/invoice">Invoice</Link>
+          </Menu.Item>
+          <Menu.Item key="3" icon={<NotificationOutlined />}>
+            <Link to="/admin/notifications">Notifications</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{ fontSize: "16px", width: 64, height: 64 }}
-          />
-          <span style={{fontWeight:"bold", fontSize:'20px'}}>HANDOUT BILLING</span>
-        </Header>
-        <Content
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 800,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          {children}
-        </Content>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            {children}
+          </Content>
+        </Layout>
       </Layout>
     </Layout>
   );
--- src/components/routes/Route.js
+++ src/components/routes/Route.js
@@ -1,115 +1,44 @@
-import React, { createRef, Suspense } from "react";
-import lodash from "lodash";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  useNavigate,
-  useLocation,
-} from "react-router-dom";
-import NotFound from "../defaultPage/notFound";
-import myRoutes from "../routes/MyRoutes";
-import { useCheckLogin, usePageAccess } from "../auth/auth";
-import NotAllowed from "../defaultPage/notAllowed";
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import CustomLayout from '../../components/layout/layout';
+import NotFound from '../defaultPage/notFound';
+import Products from '../../components/pages/products';
 import Login from "../defaultPage/login";
 import { ProjectName } from "../common/constants";
 
-export const historyRef = createRef();
-
 const Router = () => {
-  let paths = [];
-
-  // Utility function to retrieve all paths from routes configuration
-  const getPaths = (routes, parent_keys = []) => {
-    for (let i = 0; i < routes.length; i++) {
-      let _route = routes[i];
-      let _key = _route.key;
-      let _parent_keys = [...parent_keys, _key];
-      if (_route.path) {
-        paths.push({ path: _route.path, keys: _parent_keys });
-      } else if (_route.children) {
-        paths = getPaths(_route.children, _parent_keys);
-      }
-    }
-    return paths;
-  };
-
-  paths = getPaths(myRoutes, []);
-
-  // Component to handle route authentication and access
-  const MyRoute = ({ my_route_key, authenticate, children }) => {
-    const { pathname } = useLocation();
-    let url = pathname.split("/");
-    if (url[1]) {
-      let path = lodash.startCase(lodash.camelCase(url[1]));
-      document.title = path + " | " + ProjectName;
-    } else {
-      document.title = ProjectName;
-    }
-
-    const isLoggedIn = useCheckLogin();
-    const hasAccess = usePageAccess(my_route_key);
-    console.log("isLoggedIn",isLoggedIn)
-    console.log("hasAccess",hasAccess)
-
-    if (!authenticate) {
-      return <>{children}</>;
-    }
-
-    if (!isLoggedIn && !hasAccess) {
-      return <>{children}</>;
-    }
-
-    if (!isLoggedIn) {
-      return <Login />; // Redirect to Login component if not logged in
-    }
-
-    if (!hasAccess) {
-      return <NotAllowed />; // Redirect to NotAllowed component if no access
-    }
-  };
-
-  // Function to render nested routes recursively
-  const renderRoutes = (routes) => {
-    return routes.map((route) => {
-      const { key, path, component: Component, children } = route;
-
-      return (
-        <Route
-          key={key}
-          path={path}
-          element={
-            <MyRoute
-              my_route_key={key}
-              authenticate={route.authenticate}
-            >
-              <Component />
-            </MyRoute>
-          }
-        >
-          {children && renderRoutes(children)}
-        </Route>
-      );
-    });
-  };
-
-  const BrowserRoute = () => {
-    const history = useNavigate();
-    historyRef.current = history;
-    return (
-      <Routes>
-        {renderRoutes(myRoutes)}
-        <Route path="*" element={<NotFound />} /> {/* Handle 404 */}
-      </Routes>
-    );
-  };
-
   return (
     <BrowserRouter>
-      <Suspense fallback={<div />}>
-        <BrowserRoute />
-      </Suspense>
+      <RoutePages />
     </BrowserRouter>
+  );
+};
+
+const RoutePages = () => {
+  const location = useLocation();
+  useEffect(() => {
+    const pathname = location.pathname;
+    const cleanedPathname = pathname.replace(/\//, '').replace(/\/$/, '');
+   let LocationName = `${cleanedPathname} | ${ProjectName}`;
+    document.title = LocationName.toUpperCase();
+  }, [location.pathname]); 
+
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route
+        path="*"
+        element={
+          <CustomLayout>
+            <Routes>
+              <Route path="/invoice" element={<Products />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </CustomLayout>
+        }
+      />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
 
